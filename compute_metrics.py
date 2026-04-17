7e834a76784f8c516ad54161b709e0c34599e104
binder-badge <124085323+binder-badge@users.noreply.github.com>
2026-04-16 23:18:20 -0400

    did data 4-8, time 3

--- compute_metrics.py
+++ compute_metrics.py
@@ -20,7 +20,47 @@ def compute(packets):
 	# dist
 	avg_hops=0
 
-	result = dict()
-	for packet in packets:
-		
-		
+	# others/helper vars
+	results = dict()
+
+	# done under the assumption that the packets have already been filtered and parsed, leaving only ICMP ping exchange packets. 
+	# this means that the first packet is likely to be an echo request coming from the node in question
+	node_ip = packets[0]['src_ip']
+
+	print(node_ip)
+	for i in range(len(packets)):
+		# if is request
+		if packet[i]['icmp_type'] == 8:
+			if packet[i]['src_ip'] == node_ip:
+				# total length header = ipv4 header + packet
+				# + 14 because ethernet header itself is 14 bytes (6 for both src and dst macs + 2 for type), thus getting data of whole frame
+				sent_request_data += int(packet[i]["length"])
+				sent_request_bytes += int(packet[i]["length"]) + 14
+
+				# if the current packet is a request coming from the node's ip, there is an accomanying reply
+				avg_reply_delay += (packet[i+1]["timestamp"]-packet[i]["timestamp"])
+				
+				
+			else:
+				received_request_data += int(packet[i]["length"])
+				received_request_bytes += int(packet[i]["length"]) + 14
+
+
+	request_goodput /= (packets[len(packets)-1]["timestamp"] - packets[0]["timestamp"])
+	avg_reply_delay /= sent_requests
+
+	# note to self ask if requests sent by nodes is used in this or requests both sent or received
+	# avg_hops /= (sent_requests + 
+
+	results.update({"Request Bytes Sent":sent_request_bytes})
+	results.update({"Request Bytes Received":received_request_bytes})
+	results.update({"Request Data Sent":sent_request_data})
+	results.update({"Request Data Received":received_request_data})
+
+	results.update({"Request Goodput (kB/sec)":request_goodput})
+	results.update({"Average Reply Delay (us)":avg_reply_delay})
+	
+	results.update({"Average Request Hop Count":avg_hops})
+	#print(str(sent_request_bytes) + " " + str(received_request_bytes))
+	#print(str(sent_request_data) + " " + str(received_request_data))
+	return results
--- packet_analyzer.py
+++ packet_analyzer.py
@@ -10,29 +10,3 @@ with open("results.csv","w") as file:
 	# compile results and send to csv
 	# probably a more elegant way but CBA
 	# yes, this is ugly
-	line = node_num + ",Size,Requests Sent," + sent_requests + "\n"
-	file.write(line)
-	line = node_num + ",Size,Requests Received," + received_requests + "\n"
-	file.write(line)
-	line = node_num + ",Size,Replies Sent," + sent_replies + "\n"
-	file.write(line)
-	line = node_num + ",Size,Replies Received," + received_replies + "\n"
-	file.write(line)
-	line = node_num + ",Size,Request Bytes Sent," + sent_request_bytes + "\n"
-	file.write(line)
-	line = node_num + ",Size,Request Bytes Received," + received_request_bytes + "\n"
-	file.write(line)
-	line = node_num + ",Size,Request Data Sent," + sent_request_data + "\n"
-	file.write(line)
-	line = node_num + ",Size,Request Data Received," + received_request_data + "\n"
-	file.write(line)
-	line = node_num + ",Time,Average RTT (ms)," + avg_rtt + "\n"
-	file.write(line)
-	line = node_num + ",Time,Request Throughput (kB/sec)," + request_throughput + "\n"
-	file.write(line)
-	line = node_num + ",Time,Request Goodput (kB/sec)," + request_goodput + "\n"
-	file.write(line)
-	line = node_num + ",Time,Average Reply Delay (us)," + avg_reply_delay + "\n"
-	file.write(line)
-	line = node_num + ",Distance,Average Request Hop Count," + avg_hops + "\n"
-	file.write(line)
