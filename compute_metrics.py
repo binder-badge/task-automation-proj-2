ae835b8c99dd477af190efedcb9c242f64fb1ced
binder-badge <124085323+binder-badge@users.noreply.github.com>
2026-04-20 23:31:33 -0400

    update computer

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,13 +1,14 @@
 import re
 from pathlib import Path
 
-def compute(packets):
+def compute(packets,node_ip):
 	# init vars
 	# data
 	sent_requests=0
 	received_requests=0
 	sent_replies=0
 	received_replies=0
+
 	sent_request_bytes=0
 	received_request_bytes=0
 	sent_request_data=0
@@ -21,46 +22,51 @@ def compute(packets):
 	avg_hops=0
 
 	# others/helper vars
+	default_ttl = 128 
 	results = dict()
 
-	# done under the assumption that the packets have already been filtered and parsed, leaving only ICMP ping exchange packets. 
-	# this means that the first packet is likely to be an echo request coming from the node in question
-	node_ip = packets[0]['src_ip']
-
-	print(node_ip)
 	for i in range(len(packets)):
 		# if is request
-		if packet[i]['icmp_type'] == 8:
-			if packet[i]['src_ip'] == node_ip:
-				# total length header = ipv4 header + packet
-				# + 14 because ethernet header itself is 14 bytes (6 for both src and dst macs + 2 for type), thus getting data of whole frame
-				sent_request_data += int(packet[i]["length"])
-				sent_request_bytes += int(packet[i]["length"]) + 14
+		if packets[i]['icmp_type'] == 8:
+			packet_size=int(packets[i]["length"])
 
-				# if the current packet is a request coming from the node's ip, there is an accomanying reply
-				avg_reply_delay += (packet[i+1]["timestamp"]-packet[i]["timestamp"])
-				
+			# check if request was received or sent. 
+			# src = node = sent, otherwise, received
+			if packets[i]['src_ip'] == node_ip:
+				# 42 = ethernet + ipv4 + icmp headers
+				sent_request_data += packet_size - 42
+				sent_request_bytes += packet_size 
+
 				
 			else:
-				received_request_data += int(packet[i]["length"])
-				received_request_bytes += int(packet[i]["length"]) + 14
+				received_request_data += packet_size - 42
+				received_request_bytes += packet_size
+				
+				# if the current packet is a request coming from not node's ip, there is an accomanying reply
+				avg_reply_delay += (packets[i+1]["timestamp"]-packets[i]["timestamp"])
+
+				# only calculate hops of requests received. if it was sent, the ttl is set to 128 because it was made by the node and hasn't "travelled" anywhere
+				packet_ttl = int(packets[i]['info'].split(",")[2])
+				avg_hops = default_ttl - packet_ttl
+
+			request_goodput += packet_size
 
 
-	request_goodput /= (packets[len(packets)-1]["timestamp"] - packets[0]["timestamp"])
-	avg_reply_delay /= sent_requests
+	request_goodput /= float(packets[len(packets)-1]["timestamp"])
+	# avg_reply_delay /= sent_requests
 
 	# note to self ask if requests sent by nodes is used in this or requests both sent or received
-	# avg_hops /= (sent_requests + 
+	avg_hops /= (sent_requests + received_requests) 
 
-	results.update({"Request Bytes Sent":sent_request_bytes})
-	results.update({"Request Bytes Received":received_request_bytes})
-	results.update({"Request Data Sent":sent_request_data})
-	results.update({"Request Data Received":received_request_data})
+	results.update({"Request Bytes Sent":sent_request_bytes}) # done
+	results.update({"Request Bytes Received":received_request_bytes}) # done
+	results.update({"Request Data Sent":sent_request_data}) # done
+	results.update({"Request Data Received":received_request_data}) # done
 
-	results.update({"Request Goodput (kB/sec)":request_goodput})
-	results.update({"Average Reply Delay (us)":avg_reply_delay})
+	results.update({"Request Goodput (kB/sec)":request_goodput}) # done
+	results.update({"Average Reply Delay (us)":avg_reply_delay}) # done?
 	
-	results.update({"Average Request Hop Count":avg_hops})
+	results.update({"Average Request Hop Count":avg_hops}) 
 	#print(str(sent_request_bytes) + " " + str(received_request_bytes))
 	#print(str(sent_request_data) + " " + str(received_request_data))
 	return results
